b1d4f17d8842ebc00ed70942ea1d51caefb1a534
sheheryarkhan501 <ambidextrousfuturetech@gmail.com>
2024-03-15 21:58:00 +0500

    files renaming

---
+++ solutions/task_10/task_10.js
@@ -0,0 +1,6 @@
+"use strict";
+// Shehreyar Khan, 15th March 2024
+// This function takes three numbers and returns their sum
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
---
+++ solutions/task_10/task_10.ts
@@ -0,0 +1,7 @@
+
+// Shehreyar Khan, 15th March 2024
+// This function takes three numbers and returns their sum
+function addThreeNumbers(a: number, b: number, c: number): number {
+    return a + b + c;
+}
+
