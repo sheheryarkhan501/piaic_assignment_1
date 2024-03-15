733f5daad49866c62f328ca1d7f0bdd33d0888e0
sheheryarkhan501 <ambidextrousfuturetech@gmail.com>
2024-03-15 21:31:42 +0500

    task_5 done

---
+++ solutions/task_5/task_5.js
@@ -0,0 +1,4 @@
+"use strict";
+const newQuote = "The greatest glory in living lies not in never falling, but in rising every time we fall.";
+const famous_person = "Nelson Mandela";
+console.log(`${famous_person} once said, "${newQuote}"`);
